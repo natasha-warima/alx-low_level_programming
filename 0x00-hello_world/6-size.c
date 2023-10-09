3373586425088db70be994c354a1e55d570ad600
Natasha Warima <natasha.warima@gmail.com>
2023-10-09 10:55:58 -0700

    10th coomit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
 
 int main(void)
 {
-printf("Size of a char: %1d byte(s)\n", (sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	char A;
+	int B;
+	long int C;
+	long long int D;
+	float E;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(E));
 return (0);
 }

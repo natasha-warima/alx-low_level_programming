bb1b72ea84598c0e846c0d831fdf53955abbb44e
Natasha Warima <natasha.warima@gmail.com>
2023-10-09 09:23:43 -0700

    7th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  A program that prints a line with the printf function
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

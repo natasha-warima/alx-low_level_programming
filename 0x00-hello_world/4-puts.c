49b2182ceb7d9c193382d3b1d38b686d47c382a2
Natasha Warima <natasha.warima@gmail.com>
2023-10-09 09:00:34 -0700

    6th commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program to print line using the puts function
+ * Return:0(Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+		return (0);
+}

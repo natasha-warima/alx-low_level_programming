f8b9557888dc94766e5094b4c511db32afb134a2
Natasha Warima <natasha.warima@gmail.com>
2023-10-09 10:31:03 -0700

    8th commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This program prints the size of various types on the computer it is compiled and run on
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char A;
+	int B;
+	long int C;
+	long long int D;
+	float E;
+
+printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of a float:%lu byte(s)\n", (unsigned long)sizeof(E));
+return (0);
+}

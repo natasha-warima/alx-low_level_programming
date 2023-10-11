6eab641c6a23a34de81afcf7cef465d1a648b3f7
Natasha Warima <natasha.warima@gmail.com>
2023-10-11 10:18:30 -0700

    5th

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Prints out the alphabet in lowercase between a-z
+ * return: 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z"; letter ++)
+		putchar(letter);
+	putchat('\n');
+
+	return (0);
+}

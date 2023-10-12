ffdfd07e235c38dff70726b938726e20f88eb759
Natasha Warima <natasha.warima@gmail.com>
2023-10-12 16:36:29 -0700

    9th

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers
+ * using _putchar
+ * Description: prints 10 times the number
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y  >= 10)
+				_putchar('1');
+
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

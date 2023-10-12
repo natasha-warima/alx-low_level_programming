037185f2132d4b82f5049da68ce9342ecbff9031
Natasha Warima <natasha.warima@gmail.com>
2023-10-12 14:45:44 -0700

    5th

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - digit inputted
+ * @a: first variable
+ * @b: second variable
+ * Return: answer of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

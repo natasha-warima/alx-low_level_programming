de80ab6dda030af6e402065ecf9204cf72aaa836
Natasha Warima <natasha.warima@gmail.com>
2023-10-12 16:10:58 -0700

    7th

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ * Description: Print most with _putchar
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+		for(c = '0'; c <= '9'; c++)
+		{
+			if (!(c=='2' || c=='4'))
+				_putchar(c);
+		}
+	_putchar('\n');
+}
+

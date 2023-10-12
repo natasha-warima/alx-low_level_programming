c126002833085d43819f46dffc04521d50ce0a60
Natasha Warima <natasha.warima@gmail.com>
2023-10-12 14:34:15 -0700

    4th

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - function to check digits
+ * @c: int to check
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

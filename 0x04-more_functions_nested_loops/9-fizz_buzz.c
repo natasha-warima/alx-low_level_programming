887f2673103e7a1987fa13470b292247116d1b33
Natasha Warima <natasha.warima@gmail.com>
2023-10-12 18:06:31 -0700

    14th

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:prints 1 to 100 but
+ * for multiples of 3 replace with
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 1; g <= 100; g++)
+	{
+		if (g % 3 == 0 && g % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (g % 3 == 0 && g % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (g == 1)
+		{
+			printf("%d", g);
+		}
+		else
+		printf("%d", g);
+	}
+	printf("\n");
+
+	return (0);
+}
+

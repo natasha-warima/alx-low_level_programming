98ef22f49ddf685d910fc94d8460ddaa1150f624
Natasha Warima <natasha.warima@gmail.com>
2023-10-12 17:06:37 -0700

    10th

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints line
+ * @n: number of times char _ gets printed
+ * Description: \n printed if n is zero or less
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a;
+		
+		for (a = 1; a <= n; a++)
+	{
+		_putchar('_');
+	}
+	}
+}

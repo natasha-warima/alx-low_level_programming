f84ee531e729904f7e4322eca5e8e508aab01a5b
Natasha Warima <natasha.warima@gmail.com>
2023-10-16 08:51:43 -0700

    1st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that updates value of pointer to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Description of 0x05-pointers,arrays,strings repository

586764b95144f6d7a23772a8cbca6e830d3e5b96
Natasha Warima <natasha.warima@gmail.com>
2023-10-18 06:14:29 -0700

    Using strncpy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * strncpy - copies string upto n bytes
+ * @dest: original string
+ * @src: string being copied
+ * @n: most n bytes that are copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int c;
+
+	c = 0;
+	while (c < n && src[c] != '\0')
+	{
+		dest[c] = src[c];
+		c++;
+	}
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+	return (dest);
+}
